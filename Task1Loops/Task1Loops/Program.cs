aeead476583787d18d44d7f379faeb7a082003d7
GunashMammadli <146819479+GunashMammadli@users.noreply.github.com>
2023-10-10 02:47:50 +0400

    Done: Task1

---
+++ Task1Loops/Task1Loops/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Task1Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1
+            //int number = Convert.ToInt32(Console.ReadLine()); 
+            //int count = 0;
+            //for (int i = 1; i <= number; i++) {
+            //    if (number % i == 0) {
+            //        count++; 
+            //    }           
+            //}
+            //if (number == 0 || number == 1) Console.WriteLine(number + " is not a prime or composite number.");
+            //if (count == 2) Console.WriteLine(number + " is a prime number.");
+            //else Console.WriteLine(number + " is a composite number.");
+
+
+
+            //Task2
+            //int number = Convert.ToInt32(Console.ReadLine());
+
+            //FOR
+
+            //for (int i = 0; number % 2 == 00 && number > 0; i++) {
+            // number = number / 2; 
+            //}
+            //if (number == 1) Console.WriteLine(true);
+            //else Console.WriteLine(false);
+
+
+            //WHILE
+
+            //int i = 0;
+            //while (number % 2 == 00 && number > 0) {
+            //    number = number / 2;
+            //    i++;}
+            //if (number == 1) Console.WriteLine(true);
+            //else Console.WriteLine(false);
+
+
+
+
+            //Task3 
+            // :(
+        }
+    }
+}
